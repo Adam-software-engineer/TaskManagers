3ba6c96d7fdb8062dd7e8080f035e51a3e910548
Adam Steinberg <126136311+javascriptforlife66@users.noreply.github.com>
2024-01-27 14:00:04 -0600

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,20 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Adam Steinberg on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
